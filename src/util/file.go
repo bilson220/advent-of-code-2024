a793f9269f4905f16770e2339771d8f54b156e93
Billy Wilson <billywilson220@gmail.com>
2024-12-06 19:00:56 -0700

    add base LineReader

--- src/util/file.go
+++ src/util/file.go
@@ -5,24 +5,26 @@ import (
 	"os"
 )
 
-// ReadFileLines reads a file line by line and returns an array of strings.
-// Returns an error if the file cannot be read.
-func ReadFileLines(filePath string) ([]string, error) {
+type LineReader[T any] interface {
+	onLine(line string)
+	collect() *T
+}
+
+func ReadFileLines[T any](filePath string, reader LineReader[T]) (*T, error) {
 	file, err := os.Open(filePath)
 	if err != nil {
 		return nil, err
 	}
 	defer file.Close()
 
-	var lines []string
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
+		reader.onLine(scanner.Text())
 	}
 
 	if err := scanner.Err(); err != nil {
 		return nil, err
 	}
 
-	return lines, nil
+	return reader.collect(), nil
 }
